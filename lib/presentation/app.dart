ca9c0c60c69f87b6f8c71f5cb302100366e52914
Hümbət Hümbətov <hhumbetovv@gmail.com>
2023-08-21 00:05:24 +0400

    EasyLocalization changed to Vanilla Localization

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,7 +1,9 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../constants/themes.dart';
+import '../cubits/cubit/settings_cubit.dart';
 import 'routers/app_router.dart';
 
 class App extends StatelessWidget {
@@ -11,19 +13,33 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: "appTitle".trExists() ? "appTitle".tr() : "Rick and Morty",
-      theme: AppThemes.light,
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      builder: (context, child) {
-        final MediaQueryData data = MediaQuery.of(context);
-        return MediaQuery(
-          data: data.copyWith(textScaleFactor: data.textScaleFactor > 1.5 ? 1.5 : data.textScaleFactor),
-          child: child ?? const SizedBox(),
+    final l10n = AppLocalizations.of(context);
+    return BlocConsumer<SettingsCubit, SettingsState>(
+      listener: (context, state) {
+        if (state.failure != null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.failure?.message ?? l10n?.error ?? ''),
+            ),
+          );
+        }
+      },
+      builder: (context, state) {
+        return MaterialApp.router(
+          title: AppLocalizations.of(context)?.appTitle ?? "Rick and Morty",
+          theme: AppThemes.light,
+          debugShowCheckedModeBanner: false,
+          routerConfig: AppRouter.router,
+          supportedLocales: AppLocalizations.supportedLocales,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          locale: state.locale,
+          builder: (context, child) {
+            final MediaQueryData data = MediaQuery.of(context);
+            return MediaQuery(
+              data: data.copyWith(textScaleFactor: data.textScaleFactor > 1.5 ? 1.5 : data.textScaleFactor),
+              child: child ?? const SizedBox(),
+            );
+          },
         );
       },
     );
